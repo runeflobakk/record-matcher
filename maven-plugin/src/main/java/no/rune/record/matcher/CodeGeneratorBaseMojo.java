3fcb2eab7141ed3b50cf72450495e551d78bbf2b
Rune Flobakk <flobakk@gmail.com>
2026-03-01 21:23:54 +0100

    Split output dir preparation into separate mojo
    The reason being that the record analyzing for code generation is based
    on reflection, i.e. actually loading the classes to figure out how to
    generate corresponding Matchers, and for IDEs able to integrate plugin
    executions as part of their integral build processes, will typically
    execute within the runtime JVM of the IDE, which may be incompatible
    (i.e. earlier version) than the compilation target of the records to
    load.
    
    But creating the output directory and adding it as a test source folder
    is still desireable to happen within the build of the IDE, so doing this
    as a separate goal makes the IDE "discover" the output directory
    automatically.
    
    The generate goal will inform about this change if the output directory
    has not been created, and suggest adding the goal to the plugin
    execution configuration.

---
+++ maven-plugin/src/main/java/no/rune/record/matcher/CodeGeneratorBaseMojo.java
@@ -0,0 +1,41 @@
+package no.rune.record.matcher;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Set;
+
+abstract class CodeGeneratorBaseMojo extends AbstractMojo {
+
+    protected static final String PLUGIN_CONF_PROP_PREFIX = "recordmatcher.";
+
+    /**
+     * The directory where the generated code will be written to.
+     */
+    @Parameter(required = true,
+            defaultValue = "${project.build.directory}/generated-test-sources/record-matchers",
+            property = PLUGIN_CONF_PROP_PREFIX + "outputDirectory")
+    private File outputDirectory;
+
+    /**
+     * Project packaging types where execution is skipped.
+     */
+    @Parameter(required = true,
+            defaultValue = "pom",
+            property = PLUGIN_CONF_PROP_PREFIX + "skipForPackaging")
+    protected Set<String> skipForPackaging;
+
+
+    @Parameter(required = true, readonly = true,
+            defaultValue = "${project}")
+    protected MavenProject mavenProject;
+
+
+
+    Path resolveOutputDirectory() {
+        return outputDirectory.toPath();
+    }
+}
--- maven-plugin/src/main/java/no/rune/record/matcher/GenerateRecordMatcherMojo.java
+++ maven-plugin/src/main/java/no/rune/record/matcher/GenerateRecordMatcherMojo.java
@@ -1,6 +1,5 @@
 package no.rune.record.matcher;
 
-import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -17,13 +16,13 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
 
+import static java.nio.file.Files.isDirectory;
 import static java.util.Objects.requireNonNullElseGet;
 import static java.util.function.Predicate.not;
 import static java.util.stream.Collectors.joining;
@@ -33,11 +32,15 @@ import static no.rune.record.matcher.ScanHelper.isAccessibleFromSamePackage;
 import static org.apache.maven.plugins.annotations.LifecyclePhase.GENERATE_TEST_SOURCES;
 import static org.apache.maven.plugins.annotations.ResolutionScope.COMPILE;
 
-@Mojo(name = "generate", defaultPhase = GENERATE_TEST_SOURCES, requiresDependencyResolution = COMPILE)
-public class GenerateRecordMatcherMojo extends AbstractMojo {
+@Mojo(
+        name = GenerateRecordMatcherMojo.GOAL_NAME,
+        defaultPhase = GENERATE_TEST_SOURCES,
+        requiresDependencyResolution = COMPILE)
+public class GenerateRecordMatcherMojo extends CodeGeneratorBaseMojo {
+
+    static final String GOAL_NAME = "generate";
 
     private static final Logger LOG = LoggerFactory.getLogger(GenerateRecordMatcherMojo.class);
-    private static final String PLUGIN_CONF_PROP_PREFIX = "recordmatcher.";
 
     /**
      * Specifies the fully qualified class names of the records to
@@ -73,37 +76,6 @@ public class GenerateRecordMatcherMojo extends AbstractMojo {
     private Set<String> excludes;
 
 
-    /**
-     * The directory where the generated Hamcrest matchers will be written to.
-     */
-    @Parameter(required = true,
-            defaultValue = "${project.build.directory}/generated-test-sources/record-matchers",
-            property = PLUGIN_CONF_PROP_PREFIX + "outputDirectory")
-    private File outputDirectory;
-
-
-    /**
-     * Whether the {@link #outputDirectory} should be included as a test sources root.
-     */
-    @Parameter(required = true,
-            defaultValue = "true",
-            property = PLUGIN_CONF_PROP_PREFIX + "includeGeneratedCodeAsTestSources")
-    private boolean includeGeneratedCodeAsTestSources;
-
-
-    /**
-     * Project packaging types where execution is skipped.
-     */
-    @Parameter(required = true,
-            defaultValue = "pom",
-            property = PLUGIN_CONF_PROP_PREFIX + "skipForPackaging")
-    private Set<String> skipForPackaging;
-
-
-    @Parameter(required = true, readonly = true,
-            defaultValue = "${project}")
-    private MavenProject mavenProject;
-
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
@@ -113,18 +85,7 @@ public class GenerateRecordMatcherMojo extends AbstractMojo {
         }
 
         Path outputDirectory = resolveOutputDirectory();
-        try {
-            Files.createDirectories(outputDirectory);
-            LOG.info("Generating Hamcrest matchers in {}", outputDirectory);
-        } catch (IOException e) {
-            throw new UncheckedIOException(
-                    "Unable to create output directory " + outputDirectory + ", " +
-                    "because " + e.getClass().getSimpleName() + ": " + e.getMessage(), e);
-        }
-        if (includeGeneratedCodeAsTestSources) {
-            mavenProject.addTestCompileSourceRoot(outputDirectory.toString());
-            LOG.debug("{} has been added as a compiler test sources directory", outputDirectory);
-        }
+        LOG.info("Generating matchers in {}", outputDirectory);
 
         var generator = new RecordMatcherGenerator();
         var writtenFiles = resolveIncludedRecords()
@@ -154,8 +115,36 @@ public class GenerateRecordMatcherMojo extends AbstractMojo {
 
     }
 
-    private Path resolveOutputDirectory() {
-        return outputDirectory.toPath();
+    @Override
+    protected Path resolveOutputDirectory() {
+        Path outputDirectory = super.resolveOutputDirectory();
+        if (!isDirectory(outputDirectory)) {
+            throw new IllegalStateException(
+                    "Directory " + outputDirectory + " does not exist. " +
+                    """
+                    From record-matcher-maven-plugin version >= 0.4.0, it is required to configure the \
+                    goal 'prepare-output-directory' to create this directory separately before the \
+                    'generate' goal is run. Please ensure the plugin's execution is configured like this:
+
+                    <executions>
+                        <execution>
+                            <goals>
+                                <goal>prepare-output-directory</goal>
+                                <goal>generate</goal>
+                            </goals>
+                        </execution>
+                    <execution>
+
+                    If you are running the goal directly from the command line, you can try prepending \
+                    the generate goal with the prepare-output-directory goal:
+
+                     mvn record-matcher:prepare-output-directory record-matcher:generate
+
+                    The README at https://github.com/runeflobakk/record-matcher further explains how to \
+                    run standalone goals of the record-matcher-maven-plugin.
+                    """);
+        }
+        return outputDirectory;
     }
 
 
